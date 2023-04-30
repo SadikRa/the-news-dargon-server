d2962234f52e9f60fd06f78cb0bf75eec03e0e43
SadikRa <sadikrahman494@gmail.com>
2023-04-30 17:49:39 +0600

    added vircel

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
 
-const app = express()
-const port = process.env.Port || 5000;
-
-
-
-const categories = require('./data/categories.json')
-
-app.get('/app' , (req, res) =>{
-        res.send('dargon is rani')
-})
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.use(cors());
 
-app.get('/categories' , (req, res) =>{
-    res.send(categories)
+app.get('/', (req, res) => {
+    res.send('Dragon is running')
+});
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
 })
 
-app.listen(port, () =>{
-    console.log(`dragon api is run ${port}`)
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id);
+        res.send(categoryNews)
+    }
+
+})
+
+app.listen(port, () => {
+    console.log(`Dragon API is running on port: ${port}`)
 })
